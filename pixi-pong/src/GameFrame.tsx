5bd89c8697d8197004280aa1b57eb436e45ba409
Theo <theo.acooper@gmail.com>
2019-06-11 20:05:36 -0700

    finished base code,  need to fix win/tie state

--- pixi-pong/src/GameFrame.tsx
+++ pixi-pong/src/GameFrame.tsx
@@ -1,10 +1,12 @@
 import * as React from 'react';
 import * as Pixi from 'pixi.js';
 
-const windowbounds: number[] = [600, 450];
-const bSize = 20;
-const pSize = [10, 70];
-const pOffset = 3;
+//constants:
+const windowbounds: number[] = [700, 500];
+const bSize = 16;
+const pSize = [16, 90];
+const pOffset = 30;
+//TODO: add sounds on collisions and various events
 
 class pongball {
 	g: Pixi.Graphics
@@ -37,35 +39,34 @@ class pongball {
 		// Collision detection //
 		let bouncing = false; // can't bounce on multiple things in one frame
 		// it can't bounce on the same object within 5 frames
-		
+
 		// floor collision:
-		if(this.fBounceInvin===0){
+		if (this.fBounceInvin === 0) {
 			if (this.rectBounds[0][1] < 0 || this.rectBounds[1][1] > windowbounds[1]) {
 				console.log("floor coll")
 				this.Vy = -this.Vy * 1.1; // invert Vy on ceiling or floor collision
 				this.fBounceInvin = 5; // set invin frames
 				bouncing = true;
-			}	
+			}
 		}
-		if(this.pBounceInvin === 0 && !bouncing){
+
+		if (this.pBounceInvin === 0 && !bouncing) {
 			//		  Bounces off left paddle logic: it needs to be on or behind the paddle and within the rectangle height of the paddle
 			if ((this.rectBounds[0][0] <= pSize[0] + pOffset) && (PaddleYPos_both[0] <= this.rectBounds[0][1] && this.rectBounds[1][1] <= PaddleYPos_both[0] + pSize[1])) {
 				console.log("P1 hit")
 				this.Vx = -this.Vx * 1.1 // invert Vx on paddle collision
-				this.pBounceInvin = 5 // set invin frames
+				this.pBounceInvin = 15 // set invin frames
 				bouncing = true;
 			}
 			//		  Bounces off right paddle logic:
 			else if ((this.rectBounds[0][0] >= windowbounds[0] - (pSize[0] + pOffset)) && (PaddleYPos_both[1] <= this.rectBounds[0][1] && this.rectBounds[1][1] <= PaddleYPos_both[1] + pSize[1])) {
 				console.log("P2 hit")
 				this.Vx = -this.Vx
-				this.pBounceInvin =5;
+				this.pBounceInvin = 15;
 			}
 		}
-
 		// sets P1L or P2L cFlag when it hits goals:
-		// cant be bouncing and trigger goal collision at same time
-		if ((this.rectBounds[0][0] < 0 || this.rectBounds[1][0] > windowbounds[0])&&!bouncing) {
+		if (this.rectBounds[0][0] < 0 || this.rectBounds[1][0] > windowbounds[0]) {
 			if (!this.cFlag) {
 				console.log("wall coll");
 				this.cFlag = (this.rectBounds[0][0] < 0 ? "P1" : "P2") + "L";
@@ -75,8 +76,12 @@ class pongball {
 			this.Vy = 0;
 			//this.Vx = -this.Vx;
 		}
-		if(this.fBounceInvin!==0){this.fBounceInvin--;}
-		if(this.pBounceInvin!==0){this.pBounceInvin--;}
+		if (this.fBounceInvin !== 0) { this.fBounceInvin--; }
+		if (this.pBounceInvin !== 0) { this.pBounceInvin--; }
+		//This regects bounces if it is past a threshold so that the ball cant bounce inside the paddle
+		if (this.rectBounds[0][0] < pOffset || this.rectBounds[1][0] > windowbounds[0] - (pOffset)) {
+			this.pBounceInvin = 15;
+		}
 	}
 
 	draw = () => {
@@ -120,6 +125,9 @@ class paddle {
 	}
 }
 
+function sleep(ms: number) {
+	return new Promise(resolve => setTimeout(resolve, ms));
+}
 
 interface IMainProps { }
 interface IMainState { }
@@ -133,26 +141,30 @@ class GameFrame extends React.Component<IMainProps, IMainState>{
 	P1score: Pixi.Text
 	P2score: Pixi.Text
 	scores: number[]
-	hasBegun: boolean
-	isPaused: boolean
+	gameOver: boolean
 	ball: pongball
 	ballVel: number
 	paddle1: paddle
 	//paddle2: paddle
-	
+
 	constructor(props: any) {
 		super(props);
 		this.props = props;
-		this.scores = [0,0];
+		this.scores = [0, 0];
 		this.G = new Pixi.Graphics();
-		this.ballVel = 3;
-		this.isPaused = false;
-		this.hasBegun = false;
+		this.ballVel = 4;
+		this.gameOver = false;
 	}
 
 	// After mounting, add the Pixi Renderer to the div and start the Application.
 	componentDidMount() {
-		this.app = new Pixi.Application({ width: windowbounds[0], height: windowbounds[1], backgroundColor: 0x000000, antialias: true });
+		this.app = new Pixi.Application({
+			width: windowbounds[0],
+			height: windowbounds[1],
+			backgroundColor: 0x000000,
+			antialias: true
+		});
+
 		this.gameCanvas.appendChild(this.app.view);
 		this.app.start(); //start renderer;
 		this.app.stage.addChild(this.G);
@@ -165,62 +177,31 @@ class GameFrame extends React.Component<IMainProps, IMainState>{
 	initGame = () => {
 		console.log("begingame called");
 		this.G.clear();
-		
-		if (!this.hasBegun) {
-			this.beginButton = new Pixi.Text('Click To Begin', {
-				fontFamily: 'Teko',
-				fontSize: 75,
-				fill: 'white',
-				align: 'center',
-			});
-			this.beginButton.anchor.set(0.5)
-			this.beginButton.position.set(windowbounds[0]/2, windowbounds[1]/2);
-			this.beginButton.interactive = true;
-			this.app.stage.addChild(this.beginButton);
 
-			this.beginButton.on('pointerdown', ()=>{
-				console.log("button triggered")
-				this.app.ticker.add((delta) => {
-					this.updateGame(delta);
-				}); //create update timer
-				this.props.buttonfunc(); //start timer in DOM
-				this.app.stage.removeChild(this.beginButton);
-				
-				this.drawScene();
-				this.drawScore();
-				//this.button.destroy(); // destroy the button
-			})
-			
-			console.log('button created')
-			this.hasBegun = true;
-		}
-		else if (this.hasBegun) {
-			this.app.ticker.stop(); //stop updates
+		this.beginButton = new Pixi.Text('Click To Begin', {
+			fontFamily: 'Teko',
+			fontSize: 75,
+			fill: 'white',
+			align: 'center',
+		});
+		this.beginButton.anchor.set(0.5)
+		this.beginButton.position.set(windowbounds[0] / 2, windowbounds[1] / 2);
+		this.beginButton.resolution = 4;
+		this.beginButton.interactive = true;
+		this.app.stage.addChild(this.beginButton);
 
-			this.ball.g.clear();
-			this.paddle1.g.clear();
-			//this.paddle2.g.clear();
+		this.beginButton.on('pointerdown', () => {
+			console.log("button triggered")
+			this.app.ticker.add((delta) => {
+				this.updateGame(delta);
+			}); //create update timer
+			this.props.buttonfunc(); //start timer in DOM
+			this.app.stage.removeChild(this.beginButton);
 
-			delete this.ball;
-			delete this.paddle1;
-			//delete this.paddle2
-
-			this.ballVel += 0.5; //each level gets faster
 			this.drawScene();
 			this.drawScore();
-			//FIXME: implement so the game doesnt immediately startover coundown 
-		}
-
-		//TODO: have paddles/ball remain the same object
-		// set the ball in a rand direction
-		let dir = Math.floor(Math.random() * 2) ? 1 : -1
-		let angle = Math.ceil(Math.random() * (Math.PI)); // send the ball at velocity ballVel in random angle
-		this.ball = new pongball({
-			size: 20,
-			pos: [300, 225],
-			Vx: this.ballVel * Math.cos(angle) * dir,
-			Vy: this.ballVel * Math.sin(angle) * dir
-		});
+			//this.button.destroy(); // destroy the button
+		})
 		this.paddle1 = new paddle({
 			xPos: pOffset
 		});
@@ -228,18 +209,108 @@ class GameFrame extends React.Component<IMainProps, IMainState>{
 		//	  xPos: windowbounds[0]-pOffset
 		//});
 
+		//DONE: have paddles/ball remain the same object
+		// set the ball in a rand direction:
+		let dir = Math.floor(Math.random() * 2) ? 1 : -1
+		let angle = Math.ceil(Math.random() * (Math.PI)); // send the ball at velocity ballVel in random angle
+		this.ball = new pongball({
+			size: 20,
+			pos: [windowbounds[0] / 2, windowbounds[1] / 2],
+			Vx: this.ballVel * Math.cos(angle) * dir,
+			Vy: this.ballVel * Math.sin(angle) * dir
+		});
+
+		this.app.stage.addChild(this.ball.g);
+		this.app.stage.addChild(this.paddle1.g);
+		//this.app.stage.addChild(this.paddle2.g);
+
+		this.app.ticker.start();
+	}
+
+	async loadStage() {
+
+		this.app.ticker.stop(); //stop updates
+		this.ball.g.clear();
+		this.app.stage.removeChild(this.ball.g);
+		delete this.ball;
+
+		this.ballVel += 0.2; //each level gets faster
+		this.drawScene();
+		this.drawScore();
+		//DONE implement countdown so the game doesnt immediately startover 
+		this.props.buttonfunc(true, false); // pause timer in DOM
+		await sleep(500);
+		this.props.buttonfunc(); // unpause timer in DOM
+
+		//DONE: have paddles/ball remain the same object
+		// set the ball in a rand direction:
+		let dir = Math.floor(Math.random() * 2) ? 1 : -1
+		let angle = Math.ceil(Math.random() * (Math.PI)); // send the ball at velocity ballVel in random angle
+		this.ball = new pongball({
+			size: 20,
+			pos: [windowbounds[0] / 2, windowbounds[1] / 2],
+			Vx: this.ballVel * Math.cos(angle) * dir,
+			Vy: this.ballVel * Math.sin(angle) * dir
+		});
+
+
 		this.app.stage.addChild(this.ball.g);
 		this.app.stage.addChild(this.paddle1.g);
 		//this.app.stage.addChild(this.paddle2.g);
 		this.app.ticker.start();
 	}
+
 	endGame = () => {
-		// WRITEME: endgame escaping
+		this.gameOver = true;
+		//DONE:? update not removed from ticker
+		this.app.ticker.remove((delta) => { this.updateGame(delta) });
+		this.app.ticker.stop()
+		this.drawScore();
+		this.props.buttonfunc(true, false)
+		if (this.scores[0] === this.scores[1]) { //tie game
+			console.log("game end on TIE")
+			const tieText1 = new Pixi.Text("TIE", {
+				fontFamily: 'Teko',
+				fontSize: 100,
+				fill: 'white',
+				align: 'center',
+			})
+			const tieText2 = new Pixi.Text("TIE", {
+				fontFamily: 'Teko',
+				fontSize: 100,
+				fill: 'white',
+				align: 'center',
+			})
+
+			tieText1.anchor.set(0.5)
+			tieText1.position.set(Math.floor(windowbounds[0] / 4), Math.floor(windowbounds[1] / 2));
+			tieText1.resolution = 1;
+			this.app.stage.addChild(tieText1);
+
+			tieText2.anchor.set(0.5)
+			tieText2.position.set(Math.floor(3 * windowbounds[0] / 4), Math.floor(windowbounds[1] / 2));
+			tieText2.resolution = 1;
+			this.app.stage.addChild(tieText2);
+		}
+		else {
+			console.log("game end on WIN")
+			const winText = new Pixi.Text("WIN", {
+				fontFamily: 'Teko',
+				fontSize: 100,
+				fill: 'white',
+				align: 'center',
+			})
+			winText.anchor.set(0.5)
+			//set to the side of the winner
+			winText.position.set(Math.floor(this.scores[0] < this.scores[1] ? windowbounds[0] / 4 : 3 * windowbounds[0] / 4), Math.floor(windowbounds[1] / 2));
+			winText.resolution = 1;
+			this.app.stage.addChild(winText);
+		}
 	}
 
 	drawScene() {
 		//NOTE: I stole this func
-		function drawDashLine(G: Pixi.Graphics, lastPosition: number[], toX: number, toY: number, dash = 16, gap = 8) {
+		function drawDashLine(G: Pixi.Graphics, lastPosition: number[], toX: number, toY: number, dash = 15, gap = 10) {
 			const currentPosition = {
 				x: Math.max(lastPosition[0], 0),
 				y: Math.max(lastPosition[1], 0)
@@ -279,7 +350,7 @@ class GameFrame extends React.Component<IMainProps, IMainState>{
 			}
 		};
 		this.G.lineStyle(10, 0xffffff);
-		this.G.moveTo(windowbounds[0] / 2, 0);
+		this.G.moveTo(windowbounds[0] / 2, 5);
 		drawDashLine(this.G, [windowbounds[0] / 2, 0], windowbounds[0] / 2, windowbounds[1]);
 		console.log('scene drawn')
 	}
@@ -288,29 +359,31 @@ class GameFrame extends React.Component<IMainProps, IMainState>{
 		this.app.stage.removeChild(this.P1score);
 		this.P1score = new Pixi.Text(this.scores[0].toString(), {
 			fontFamily: 'Teko',
-			fontSize: 50,
+			fontSize: 75,
 			fill: 'white',
 			align: 'center',
 		});
 		this.P1score.anchor.set(0.5)
-		this.P1score.position.set(Math.floor(5*windowbounds[0]/9), Math.floor(windowbounds[1]/5));
+		this.P1score.position.set(Math.floor(4 * windowbounds[0] / 7), Math.floor(windowbounds[1] / 7));
+		this.P1score.resolution = 2;
 		this.app.stage.addChild(this.P1score);
 
 		this.app.stage.removeChild(this.P2score);
 		this.P2score = new Pixi.Text(this.scores[1].toString(), {
 			fontFamily: 'Teko',
-			fontSize: 50,
+			fontSize: 75,
 			fill: 'white',
 			align: 'center',
 		});
 		this.P2score.anchor.set(0.5)
-		this.P2score.position.set(Math.floor(4*windowbounds[0]/7), Math.floor(windowbounds[1]/5));
+		this.P2score.position.set(Math.floor(3 * windowbounds[0] / 7), Math.floor(windowbounds[1] / 7));
+		this.P2score.resolution = 2;
 		this.app.stage.addChild(this.P2score);
 	}
 
-	//TODO: implement network streaming 
-	updatePaddles = (event: any) => {
-		if (this.hasBegun) this.paddle1.updatePos_mouse(event);
+	//TODO: implement DNS lookup && p2p network streaming 
+	updatePaddles = (Mevent1: any) => {
+		if (this.paddle1) { this.paddle1.updatePos_mouse(Mevent1); }
 		//this.paddle2.update(event);
 	}
 
@@ -318,9 +391,11 @@ class GameFrame extends React.Component<IMainProps, IMainState>{
 		// use delta to create frame-independent transform
 		this.ball.updatePos(delta, [this.paddle1.yPos]); //, this.Paddle2.yPos]);
 		this.drawAll();
-		if(this.ball.cFlag){
-			this.scores[this.ball.cFlag === 'P1L' ? 0:1]++;
-			this.initGame()
+		if (this.ball.cFlag) {
+			this.scores[this.ball.cFlag === 'P1L' ? 0 : 1]++;
+			//TODO: inherit win const
+			this.scores[this.ball.cFlag === 'P1L' ? 0 : 1] < 7 ? this.loadStage() : this.endGame();
+
 		}
 	}
 
@@ -336,9 +411,18 @@ class GameFrame extends React.Component<IMainProps, IMainState>{
 		this.app.stop();
 	}
 
-	handlePress = (key: any) => {
-		switch (key.charCode) {
-			//TODO: Write esc to exit game
+	//FIXME: keypress not handled
+	handlePress = (E: KeyboardEvent) => {
+		console.log("handlepress called: " + E.toString())
+		switch (E.key) {
+			
+			case "Escape":
+				if (!this.gameOver) { //only close if game is not over
+					console.log("Game Closing on Escape")
+					this.endGame();
+				}
+				break;
+
 			default: //pass
 		}
 	}
@@ -346,12 +430,14 @@ class GameFrame extends React.Component<IMainProps, IMainState>{
 	//Render the div that will contain the Pixi Renderer.
 	render() {
 		let component = this;
+		document.addEventListener('keyup', (e) => { this.handlePress(e) });
 		return (
 			<div className="GameFrameWrapper">
 				<div ref={(thisDiv: HTMLDivElement) => { component.gameCanvas = thisDiv }}
-					onMouseMove={(e) => { component.updatePaddles(e); }} />
+					onMouseMove={(e) => { component.updatePaddles(e); }}
+				/>
 			</div>
-		); //onKeyPress = {(e) => {this.handlePress(e)}}/>
+		); //
 	}
 }
 
