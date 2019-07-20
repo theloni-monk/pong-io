01a06ff057d69116b8374926fde5c7cc29447f24
Theo <theo.acooper@gmail.com>
2019-07-20 12:31:42 -0400

    updated name of referenced file

--- pixi-pong/src/App.jsx
+++ pixi-pong/src/App.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
-import GameWrapper from './GameWrapper.jsx';
+import GameLoader from './GameLoader.tsx';
+import GameWrapper from './GameWrapper.tsx';
 import logo from './assets/ttr.svg';
 import './css/App.css';
 
@@ -8,14 +9,15 @@ class App extends React.Component {
     return (
       <div className="App">
         <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Pong</h2>
+          <h2>Pong.io</h2>
+          <img src={logo} className="App-logo" alt="logo" /> 
+          
         </div>
         <p className="App-intro" >
           Basic pong recreation written to learn typescript, pixi.js, socketio, and node
         </p>
         <div className="GameBox">
-          <GameWrapper />
+          <GameLoader />
         </div>
       </div>
     );
